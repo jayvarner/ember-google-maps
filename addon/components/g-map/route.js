3e4db407ee727423f24790f8fd5951684c9e823c
Jay Varner <jayvarner@gmail.com>
2019-05-06 21:08:25 -0400

    Add abality to call setPanel() on DirectionsRenderer to display turn by turn directions.

--- addon/components/g-map/route.js
+++ addon/components/g-map/route.js
@@ -2,6 +2,7 @@ import MapComponent from './map-component';
 import layout from '../../templates/components/g-map/route';
 import { get, set } from '@ember/object';
 import { reject } from 'rsvp';
+import { assert } from '@ember/debug';
 
 /**
  * A wrapper for the google.maps.DirectionsRenderer class.
@@ -17,6 +18,7 @@ export default MapComponent.extend({
 
   _type: 'route',
   _requiredOptions: ['directions'],
+  _ignoredAttrs: ['panel'],
 
   _addComponent() {
     let options = get(this, '_options');
@@ -24,6 +26,16 @@ export default MapComponent.extend({
     if (!options.directions) {
       return reject();
     }
+
+    let directionsDisplay = new google.maps.DirectionsRenderer(options);
+    let panelElement = document.getElementById(this.panel);
+    console.log(panelElement);
+    if (this.panel && panelElement) {
+      directionsDisplay.setPanel(panelElement);
+    } else if (this.panel && !panelElement) {
+      assert(`An element with an id of ${this.panel} does not exist.`)
+    }
+
 
     set(this, 'mapComponent', new google.maps.DirectionsRenderer(options));
   }
